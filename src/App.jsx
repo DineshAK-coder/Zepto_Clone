c36583fc2332b09a42636b2a91919fed15202163
DineshAK-coder-second <akd12761@gmail.com>
2026-05-12 14:21:56 +0530

    Done with the Navbar component and also wrote the routing logic in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,19 @@ import './App.css'
 import './Login.jsx'
 
 function App() {
-    
-}
+  const [isauthenticated, setIsauthenticated] = useState(false);
+  const [cart, modifyCart] = useState([]);
+  const addToCart = (product) => modifyCart([...cart,product]);
 
+  <Router>
+    {isAuthenticated && <Navbar setIsAuthenticated = {setIsAuthenticated}/>}
+    <Routes>
+      <Route path = "/login" element = {<Login setIsAuthenticated = {setIsauthenticated}/>}/>
+      <Route path = "/products"
+      element = {isAuthenticated ? <Products/> : <Navigate to = "/login"/>}/>
+      <Route path = "/cart"
+      element = {isAuthenticated ? <Cart/> : <Navigate to = "/login"/>}/>
+    </Routes>
+  </Router>
+}
 export default App
